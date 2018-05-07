5f54333d1217cec170e097366eeeffa8902bb8ab
carmen <carmen@carmen-VirtualBox>
2018-05-08 00:49:03 +0200

    Login - Control de acceso - Eliminar archivos innecesarios

--- AlquileresVEFHM/src/main/java/es/uca/iw/AlquileresVEFHM/modelos/UsuarioDao.java
+++
@@ -1,9 +0,0 @@
-package es.uca.iw.AlquileresVEFHM.modelos;
-
-import javax.transaction.Transactional;
-
-import org.springframework.data.repository.CrudRepository;
-
-@Transactional
-public interface UsuarioDao extends CrudRepository<Usuario, Integer> {
-}
--- AlquileresVEFHM/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Pagina de inicio</h2>
-</body>
-</html>
